489e653503668380d7790a5441c8c50f43d4d25c
Marcel van Heerdt <MC.vanHeerdt@student.han.nl>
2019-04-16 19:50:58 +0200

    Rest van Kotlin demo

--- src/main/kotlin/com/mattoopie/kotlindemo/Demo.kt
+++ src/main/kotlin/com/mattoopie/kotlindemo/Demo.kt
@@ -1,5 +1,40 @@
-// Named en default arguments (test a en b)
+import java.lang.Math.random
+import java.time.LocalDate
+
+/* Named en default arguments (test a en b) */
 fun hello(name: String = "Piet"): String = "Hello $name"
 
-// Lambda's (test c)
+/* Lambda's (test c) */
 fun containsEvenNumber(collection: Collection<Int>): Boolean = collection.any { it % 2 == 0 }
+
+/* Zelf een lambda maken (test d) */
+fun doSomethingToNumber(number: Int, transform: (Int) -> Int): Int = transform(number)
+
+/* Nullability */
+fun getStringNullsafe(): String = "Text"
+
+fun getStringNullable(): String? = if (random() > 0.5) "Text" else null
+//fun checkNullabilityCompiler() {
+//    var text = getStringNullsafe()
+//    // text = getStringNullable()
+//    text = getStringNullable() ?: "Use this if null"
+//    text = getStringNullable()!! // Gooit NullPointerException als null
+//
+//    var length = getStringNullsafe().length
+//    // length = getStringNullable().length // Compileert niet
+//    length = getStringNullable()?.length ?: 0 // Als getStringNullable null is: gebruik 0, anders: gebruik het resultaat met .length
+//}
+
+/* Data classes (test e) */
+data class Persoon(
+    val naam: String,
+    val geboortedatum: LocalDate
+)
+
+/* Extension functions (test f en g) */
+fun String.addText(text: String): String = this.plus(text)
+
+fun List<Persoon>.sortByGeboortedatum() = this.sortedBy { it.geboortedatum }
+
+/* Collections (test h) */
+// Standaardlibraries
--- src/test/kotlin/com/mattoopie/kotlindemo/DemoTest.kt
+++ src/test/kotlin/com/mattoopie/kotlindemo/DemoTest.kt
@@ -1,4 +1,5 @@
 import org.assertj.core.api.Assertions.assertThat
+import java.time.LocalDate
 import kotlin.test.Test
 
 class FunctionsTest {
@@ -16,5 +17,82 @@ class FunctionsTest {
     @Test
     fun c_testLambda() {
         assertThat(containsEvenNumber(listOf(5, 6, 7))).isTrue()
+    }
+
+    @Test
+    fun d_testCustomLambda() {
+        assertThat(doSomethingToNumber(5) { it + 6 }).isEqualTo(11)
+    }
+
+    @Test
+    fun e_testDataClasses() {
+        val persoon = Persoon(
+            naam = "Kees",
+            geboortedatum = LocalDate.now()
+        )
+        assertThat(persoon.naam).isEqualTo("Kees")
+    }
+
+    @Test
+    fun f_testStringExtension() {
+        assertThat("Appel".addText("taart")).isEqualTo("Appeltaart")
+    }
+
+    @Test
+    fun g_testListExtension() {
+        val personen = listOf<Persoon>(
+            Persoon(
+                naam = "Kees",
+                geboortedatum = LocalDate.now()
+            ),
+            Persoon(
+                naam = "Piet",
+                geboortedatum = LocalDate.now().minusDays(3)
+            ),
+            Persoon(
+                naam = "Klaas",
+                geboortedatum = LocalDate.now().plusDays(5)
+            )
+        )
+        assertThat(personen.sortByGeboortedatum())
+            .extracting("naam")
+            .containsExactly("Piet", "Kees", "Klaas")
+    }
+
+    @Test
+    fun h_testCollections() {
+        val vandaag = LocalDate.now()
+        val personen = listOf(
+            Persoon(
+                naam = "Kees",
+                geboortedatum = vandaag
+            ),
+            Persoon(
+                naam = "Piet",
+                geboortedatum = vandaag.minusDays(3)
+            ),
+            Persoon(
+                naam = "Klaas",
+                geboortedatum = vandaag.plusDays(5)
+            )
+        )
+        assertThat(personen.filter { it.naam == "Piet" })
+            .containsExactly(
+                Persoon(
+                    naam = "Piet",
+                    geboortedatum = vandaag.minusDays(3)
+                )
+            )
+        assertThat(personen.map { it.naam.length }).containsExactly(4, 4, 5)
+        personen.forEachIndexed { index, persoon -> println("$index - ${persoon.naam}") }
+
+
+        val (geborenVoorVandaag, geborenVanafVandaag) = personen.partition { it.geboortedatum < vandaag }
+        assertThat(geborenVoorVandaag)
+            .extracting("naam")
+            .containsExactly("Piet")
+        assertThat(geborenVanafVandaag)
+            .extracting("naam")
+            .containsExactly("Kees", "Klaas")
     }
 }
